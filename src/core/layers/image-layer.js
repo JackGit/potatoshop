fb9f9d8212b81cf7db8ae0712f26c0e239b59153
jackgit <ygjack414@hotmail.com>
2017-06-24 10:41:45 +0800

    update

--- src/core/layers/image-layer.js
+++ src/core/layers/image-layer.js
@@ -4,6 +4,19 @@ export default class ImageLayer extends from Layer {
 
   constructor () {
     super()
+    this.imageUrl = ''
   }
 
+  // onimageloaded
+}
+
+ImageLayer.domRenderer = function () {
+  let img = document.createElement('img')
+  img.src = this.imageUrl
+  img.width = this.width
+  img.height = this.height
+  img.style.position = 'absolute'
+  img.style.top = this.position.y + 'px'
+  img.style.left = this.position.x + 'px'
+  img.style.opacity = this.alpha
 }
